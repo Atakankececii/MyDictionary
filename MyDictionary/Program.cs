f555e07475b7d21c65e3d484e34341f2344ffde4
Süleyman Atakan Keçeci <atakan_kececi48@hotmail.com>
2021-01-22 23:32:25 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _MyDictionary<int, string> items = new _MyDictionary<int, string>();
+            items.Add(1,"Kalem");
+            items.Add(2, "Bilgisayar");
+            items.Add(3, "Masa");
+            items.Add(4, "Klavye");
+            items.Add(5, "Mouse");
+            items.Add(6, "Kitap");
+            items.Add(7, "Telefon");
+
+
+            Console.WriteLine("Listedeki eleman sayısı : " + items.Count);
+
+            for (int i = 0; i <items.Count; i++)
+            {
+                Console.WriteLine(items.Keys);
+
+            }
+
+            foreach (var name in items.Values)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
---
+++ MyDictionary/_MyDictionary.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class _MyDictionary<T1,T2>
+    {
+        T1[] _keys;
+        T2[] _values;
+
+        public _MyDictionary()
+        {
+            _keys = new T1[0];
+            _values = new T2[0];
+        }
+
+        public void Add(T1 key,T2 value)
+        {
+            _keys = new T1[_keys.Length + 1];
+            _values = new T2[_values.Length + 1];
+
+            T1[] _tempKeys = _keys;
+            T2[] _tempValues = _values;
+
+            //keys değerlerini value değerlerine erişmek için kullandığımızdan dolayı for döngüsünü _keys'in üzerine kurduk.
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                _tempKeys[i] = _keys[i];
+                _tempValues[i] = _values[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+
+            
+
+
+        }
+
+
+
+
+
+  
+        public T1[] Keys
+        {
+            get { return _keys; }
+           
+        }
+
+
+        public T2[] Values
+        {
+            get { return _values; }
+
+        }
+
+     
+
+        public int Count
+        {
+            get { return _keys.Length; }
+      
+        }
+
+
+
+
+
+
+
+
+    }
+}
